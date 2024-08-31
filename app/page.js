530d89572eaeef8ade0533aeb3b13bb1afd30e23
saransrini003 <saransrini003@gmail.com>
2024-08-31 19:38:49 +0530

    text modify

--- app/page.js
+++ app/page.js
@@ -8,9 +8,11 @@ export default function HomePage() {
       <div className={styles.inner}>
         <h1 className={styles.heading}>Welcome to TAYD.</h1>
         <h2 className={styles.subheading}>Tell About Your Day</h2>
+
         <Link href="/auth" passHref>
           <span className={styles.link}>Get Started</span>
         </Link>
+        <h2 className={styles.subheading}>Made by NearCult</h2>
       </div>
     </div>
   );
