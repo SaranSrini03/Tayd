0d87175ae30635ed326500c1040bd64e756281cc
saransrini003 <saransrini003@gmail.com>
2024-09-01 23:41:05 +0530

    clerk implemented

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,5 +1,21 @@
-// app/about/page.js
-export default function AboutPage() {
-    return <h1>Login</h1>;
-  }
-  
+"use client";
+
+import { SignIn } from '@clerk/nextjs';
+
+export default function AuthPage() {
+  return (
+    <div style={styles.container}>
+      <SignIn routing="hash" />
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: '#f0f0f0', // Optional: adds a background color for better visual appeal
+  },
+};
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,19 @@
 // app/layout.js
-import './globals.css'; // Import global styles
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
+
+export const metadata = {
+  title: 'Tayd',
+  description: 'Tell About Your Day',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <ClerkProvider>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
